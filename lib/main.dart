59009e79d212dfd77ffb75ed0ebc138f282d38ca
gohdg <gohdg@naver.com>
2021-08-13 17:46:51 +0900

    customized TabController register

--- lib/main.dart
+++ lib/main.dart
@@ -24,38 +24,71 @@ class MainPage extends StatefulWidget {
   _MainPageState createState() => _MainPageState();
 }
 
-class _MainPageState extends State<MainPage> {
+class _MainPageState extends State<MainPage>
+    with SingleTickerProviderStateMixin {
+  late TabController controller;
+  int currentIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = TabController(length: 3, vsync: this);
+    controller.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('TabBar'),
-          centerTitle: true,
-          bottom: TabBar(
-            tabs: [
-              Tab(
-                text: 'Tab 1',
-                icon: Icon(Icons.home),
-              ),
-              Tab(
-                text: 'Tab 2',
-                icon: Icon(Icons.star),
-              ),
-              Tab(
-                text: 'Tab 3',
-                icon: Icon(Icons.person),
-              ),
-            ],
-          ),
-        ),
-        body: TabBarView(
-          children: [
-            FirstPage(),
-            SecondPage(),
-            ThirdPage(),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tab ${controller.index + 1}'),
+        centerTitle: true,
+        bottom: TabBar(
+          controller: controller,
+          tabs: [
+            Tab(
+              text: 'Tab 1',
+              icon: Icon(Icons.home),
+            ),
+            Tab(
+              text: 'Tab 2',
+              icon: Icon(Icons.star),
+            ),
+            Tab(
+              text: 'Tab 3',
+              icon: Icon(Icons.person),
+            ),
           ],
+        ),
+      ),
+      body: TabBarView(
+        controller: controller,
+        children: [
+          FirstPage(),
+          SecondPage(),
+          ThirdPage(),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          if (currentIndex == controller.length - 1) {
+            currentIndex = 0;
+          } else {
+            currentIndex += 1;
+          }
+          controller.animateTo(currentIndex);
+          print('aaa');
+        },
+        child: Icon(
+          Icons.add,
+          size: 32,
         ),
       ),
     );
