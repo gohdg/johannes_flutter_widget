aab9f371de939da9a824fc7cbb1623b676367874
gohdg <gohdg@naver.com>
2021-08-13 16:20:17 +0900

    init master

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(      
+      home: MainPage(),
+    );
+  }
+}
+class MainPage extends StatefulWidget {
+  const MainPage({ Key? key }) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
