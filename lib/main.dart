b468503b3ef35ecdb668504129479f8f6c2271d2
gohdg <gohdg@naver.com>
2021-08-17 14:30:07 +0900

    DropdownButton, DropdownMenuItem, items: items.map(func).toList()

--- lib/main.dart
+++ lib/main.dart
@@ -8,23 +8,58 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(      
+    return MaterialApp(
       home: MainPage(),
     );
   }
 }
-class MainPage extends StatefulWidget {
-  const MainPage({ Key? key }) : super(key: key);
 
+class MainPage extends StatefulWidget {
   @override
   _MainPageState createState() => _MainPageState();
 }
 
 class _MainPageState extends State<MainPage> {
+  final items = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5'];
+  String? value;
+
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      
-    );
-  }
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text('Dropdown Menu'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Container(
+            // width: 300,
+            margin: EdgeInsets.all(16),
+            padding: EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black, width: 4),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: DropdownButtonHideUnderline(
+              child: DropdownButton<String>(
+                value: value,
+                hint: Text('Choose item'),
+                iconSize: 36,
+                icon: Icon(
+                  Icons.arrow_drop_down,
+                  color: Colors.black,
+                ),
+                isExpanded: true,
+                items: items.map(buildMenuItem).toList(),
+                onChanged: (value) => setState(() => this.value = value),
+              ),
+            ),
+          ),
+        ),
+      );
+
+  DropdownMenuItem<String> buildMenuItem(String item) => DropdownMenuItem(
+      value: item,
+      child: Text(
+        item,
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+      ));
 }
