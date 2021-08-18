df3b0fcfc8a15d947af690a38f8354af236cec83
gohdg <gohdg@naver.com>
2021-08-18 13:41:31 +0900

    FocusScope.of(context).unfocus();, TextField-foucsNode:focusNode1, onpress: focusNode1.requestFocus();

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,23 +9,73 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(      
+    return MaterialApp(
       home: MainPage(),
     );
   }
 }
+
 class MainPage extends StatefulWidget {
-  const MainPage({ Key? key }) : super(key: key);
+  const MainPage({Key? key}) : super(key: key);
 
   @override
   _MainPageState createState() => _MainPageState();
 }
 
 class _MainPageState extends State<MainPage> {
+  FocusNode focusNode1 = FocusNode();
+
+  @override
+  void dispose() {
+    focusNode1.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hide/Show Keyboard'),
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 24, vertical: 48),
+        child: Column(
+          children: [
+            TextField(
+              autofocus: true,
+              focusNode: focusNode1,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: 'Name'),
+            ),
+            SizedBox(
+              height: 32,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                FocusScope.of(context).unfocus();
+                // SystemChannels.textInput.invokeMethod('TextInput.hide');
+              },
+              child: Text('Hide'),
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size.fromHeight(48),
+                textStyle: TextStyle(fontSize: 24),
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            ElevatedButton(
+              onPressed: () => focusNode1.requestFocus(),
+              child: Text('SHOW'),
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size.fromHeight(48),
+                textStyle: TextStyle(fontSize: 24),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
