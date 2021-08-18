416c5548cae084f2a1047de112c58353689aae9f
gohdg <gohdg@naver.com>
2021-08-18 11:54:53 +0900

    Exit App when pressed back button

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,23 +9,48 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(      
+    return MaterialApp(
       home: MainPage(),
     );
   }
 }
+
 class MainPage extends StatefulWidget {
-  const MainPage({ Key? key }) : super(key: key);
+  const MainPage({Key? key}) : super(key: key);
 
   @override
   _MainPageState createState() => _MainPageState();
 }
 
 class _MainPageState extends State<MainPage> {
+  DateTime timeBackPressed = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return WillPopScope(
+      onWillPop: () async {
+        final difference = DateTime.now().difference(timeBackPressed);
+        final isExitWarning = difference >= Duration(seconds: 2);
+
+        timeBackPressed = DateTime.now();
+
+        if (isExitWarning) {
+          final message = 'Press back again to exit';
+          Fluttertoast.showToast(msg: message, fontSize: 16.0);
+          return false;
+        } else {
+          Fluttertoast.cancel();
+          return true;
+        }
+      },
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: Text('Press Back To Exit'),
+          centerTitle: true,
+        ),
+        body: Container(),
+      ),
     );
   }
 }
